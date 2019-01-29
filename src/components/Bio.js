e0e76f7e21acb990b41c36d0837d4512329ec356
Aaron Buxbaum <me@aaronbuxbaum.com>
2019-01-28 21:46:38 -0500

    update Bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,40 +1,32 @@
-import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import { rhythm } from '../utils/typography'
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 
-function Bio() {
-  return (
-    <StaticQuery
-      query={bioQuery}
-      render={data => {
-        const { author, social } = data.site.siteMetadata
-        return (
-          <div
-            style={{
-              fontFamily: `monospace`,
-            }}
-          >
-            &#123;<br/>
-              &emsp;&emsp;author: "<strong>{author}</strong>",<br/>
-              &emsp;&emsp;email: "<a href={`mailto:${social.email}`}><strong>{social.email}</strong></a>",<br/>
-              &emsp;&emsp;linkedin: "<a href={`https://linkedin.com/in/${social.linkedin}`}><strong>{`linkedin.com/in/${social.linkedin}`}</strong></a>",<br/>
-            &#125;
-          </div>
-        )
-      }}
-    />
-  )
-}
+const bioStyles = {
+  fontFamily: 'monospace',
+  whiteSpace: 'nowrap',
+};
+
+const Bio = () => (
+  <StaticQuery
+    query={bioQuery}
+    render={data => {
+      const { author, social } = data.site.siteMetadata;
+
+      return (
+        <div style={bioStyles}>
+          &#123;<br />
+          &emsp;&emsp;author: "<strong>{author}</strong>",<br />
+          &emsp;&emsp;email: "<a href={`mailto:${social.email}`}><strong>{social.email}</strong></a>",<br />
+          &emsp;&emsp;linkedin: "<a href={`https://linkedin.com/in/${social.linkedin}`}><strong>{`linkedin.com/in/${social.linkedin}`}</strong></a>",<br />
+          &#125;
+      </div>
+      );
+    }}
+  />
+);
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-      childImageSharp {
-        fixed(width: 50, height: 50) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
     site {
       siteMetadata {
         author
@@ -45,6 +37,6 @@ const bioQuery = graphql`
       }
     }
   }
-`
+`;
 
-export default Bio
+export default Bio;
